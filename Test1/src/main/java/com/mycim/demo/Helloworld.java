8f80037444da8db1ccc172d3fe02a64cc322ebf4
Diego Ramirez <diegoramirez.dbr@gmail.com>
2020-02-17 12:28:29 -0500

    Testing committing java code.

--- Test1/src/main/java/com/mycim/demo/Helloworld.java
+++ Test1/src/main/java/com/mycim/demo/Helloworld.java
@@ -3,6 +3,6 @@ package com.mycim.demo;
 public class Helloworld {
 
     public static void main(String [] ars){
-        System.out.println("Helloworld");
+        System.out.println("Helloworld Amdocs");
     }
 }
