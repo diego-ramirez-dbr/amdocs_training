7ad61b1255c196b8e34399be6d8c0bac0f478f59
Diego Ramirez <diegoramirez.dbr@gmail.com>
2020-02-17 14:53:04 -0500

    Added Maven files and logger functionality.

--- Test1/src/main/java/com/mycim/demo/Helloworld.java
+++ Test1/src/main/java/com/mycim/demo/Helloworld.java
@@ -1,8 +1,13 @@
 package com.mycim.demo;
 
+import org.apache.log4j.Logger;
+
 public class Helloworld {
 
+    public static Logger LOG = Logger.getLogger ( "Helloworld");
+
     public static void main(String [] ars){
+        LOG.info("this is an info message");
         System.out.println("Helloworld Amdocs Dallas");
     }
 }
